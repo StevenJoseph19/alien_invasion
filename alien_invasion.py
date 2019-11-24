54680b82a7a86a19dc2c249168725002eb83dee7
Stephen Joseph Samuels <stevesam1927@gmail.com>
2019-11-24 11:22:10 +0530

    Resetting the Speed

--- alien_invasion.py
+++ alien_invasion.py
@@ -80,6 +80,7 @@ class AlienInvasion:
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
             # Reset the game statistics.
+            self.settings.initialize_dynamic_settings()
             self.stats.reset_stats()
             self.stats.game_active = True
 
@@ -144,7 +145,8 @@ class AlienInvasion:
         if not self.aliens:
         # Destroy existing bullets and create new fleet.
             self.bullets.empty()
-            self._create_fleet()    
+            self._create_fleet()  
+            self.settings.increase_speed()  
 
     def _ship_hit(self):
         """Respond to the ship being hit by an alien."""
